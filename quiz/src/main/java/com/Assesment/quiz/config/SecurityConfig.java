ef41bd5c9a5d3b661fed869d685c1ad360155ac2
sandeep-kumar-gupta <88088154+sandeep-kumar-gupta@users.noreply.github.com>
2025-11-30 13:14:10 +0530

    2nd commit with security config class and properties file

---
+++ quiz/src/main/java/com/Assesment/quiz/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.Assesment.quiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->auth.requestMatchers("/api/admin**").
+                        hasRole("ADMIN").requestMatchers("/api/**").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form ->form.disable());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailSevice(PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(User.withUsername("admin").password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN").build());
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+
+    }
+
+}
